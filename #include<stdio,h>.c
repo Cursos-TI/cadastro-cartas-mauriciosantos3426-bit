1ee4dafce70f0ec570889e01dde38b5e9ab5f769
mauriciosantos3426-bit <mauriciosouza3426@gmail.com>
2025-12-13 07:09:43 -0300

    cidade b

--- #include<stdio,h>.c
+++ #include<stdio,h>.c
@@ -1,11 +1,23 @@
 #include<stdio,h>
 int main(){
-    printf("desafo xadrez!\n");
+    printf("desafio super trunfo!\n");
     printf("carta 1")
     printf("estado a")
     printf("codigo a01")
-    char[recife]
-    int("50,000")
+    char["recife"]
+    int("50.000")
     float("2420,11 km")
     float("830.45 bilhoes de reais")
     int("35")
+
+    #include<stdio,h>
+    int main(){
+        printf("desafio super trunfo!\n")
+        printf("carta 2")
+        printf("estado b")
+        printf("codigo b02")
+        char["olinda"]
+        int("32.000")
+        float("1257,30 km")
+        float("346.22 bilhoes de reais")
+        int("30")
