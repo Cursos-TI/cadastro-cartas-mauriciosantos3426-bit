ec9104d68ed6a731ea4055ef3012d89a1ecf8138
mauriciosantos3426-bit <mauriciosouza3426@gmail.com>
2025-12-13 06:55:31 -0300

    carta 1

--- #include<stdio,h>.c
+++ #include<stdio,h>.c
@@ -1,3 +1,11 @@
 #include<stdio,h>
 int main(){
     printf("desafo xadrez!\n");
+    printf("carta 1")
+    printf("estado a")
+    printf("codigo a01")
+    char[recife]
+    int("50,000")
+    float("2420,11 km")
+    float("830.45 bilhoes de reais")
+    int("35")
